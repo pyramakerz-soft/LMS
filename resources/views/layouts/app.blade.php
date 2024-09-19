29c6437c9bd4335ff4b896a967d5e28fcce09201
= <=>
2024-09-19 14:13:54 +0300

    theme, chapter, lesson, unit, material Forms

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     @vite('resources/css/app.css')
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Pacifico&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel='stylesheet'
         href='https://cdn-uicons.flaticon.com/2.5.1/uicons-regular-rounded/css/uicons-regular-rounded.css'>
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.5.1/uicons-thin-rounded/css/uicons-thin-rounded.css'>
@@ -21,20 +20,16 @@
 </head>
 
 <body>
-    <div class="">
-        <div class="row">
-            <div class="col-md-2">
-                @yield('sidebar')
-            </div>
-            <div class="col-md-10">
-                @yield('content')
-            </div>
+    <div class="grid grid-cols-12">
+        <div class="col-span-3">
+            @yield('sidebar')
+        </div>
+        <div class="col-span-9 p-5">
+            @yield('content')
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+    @yield("login")
 </body>
 
 </html>
---
+++ resources/views/pages/login/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section("login")
+    <div class="lg:h-[100vh] flex flex-col-reverse lg:flex-row">
+        <div class="w-full lg:w-1/3 bg-white flex flex-col justify-center items-center p-12 md:p-20">
+            <img src="{{ asset('images/Paragraphcontainer.png') }}" alt="Logo" class="w-[63%]">
+            <p class="h5 text-[#111111] py-3">Hello, please sign into your account</p>
+
+            <div class="w-full text-start">
+                <label htmlFor="email" class="h-4 font-medium text-[#344054] block mb-1" >
+                    Email
+                </label>
+                <input id="email" type="email" placeholder="Enter your email" 
+                class="h5 w-full bg-white border rounded border-[#D0D5DD] shadow-sm shadow-[#1018280D] h-[22%]  py-[7.5px] px-[10.5px]"   [(ngModel)]="email">
+                
+                <label htmlFor="pass" class="h-4 font-medium text-[#344054] block mb-1 mt-4"  >
+                    Password
+                </label>
+                <input id="pass" type="password"
+                class="h5 w-full bg-white border rounded border-[#D0D5DD] shadow-sm shadow-[#1018280D] h-[22%] py-[7.5px] px-[10.5px]" [(ngModel)]="password">
+            </div>
+
+            <button class="mt-4 w-full text-white font-semibold text-xs shadow-sm bg-[#17253E] border border-[#17253E] py-2 rounded-md">Sign in</button>
+        </div>
+
+        <div class="w-full lg:w-2/3 bgLinearGradient text-white flex flex-col items-center justify-center p-3 lg:p-0">
+            <p class="text-xl md:text-4xl font-bold mb-8">Do more <span class="font-normal">with Pyramakerz</span></p>
+
+            <div class="flex flex-col items-center justify-center">
+                <img src="{{ asset('images/Layer 2.png') }}" alt="calender" class="w-[83%] mb-6">
+            </div>
+
+        </div>
+    </div>
+@endsection
