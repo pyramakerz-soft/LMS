7aa208216c6dc4158a38ea503f4d99ec323ce4a3
= <=>
2024-09-19 15:05:31 +0300

    sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,12 @@
 <div class="bg-[#17253E] min-h-[100vh] h-full border-r-[1.33px] border-[#2E3545]">
-    <div class="">
-        <div class="px-3 md:px-5 py-3 md:py-4 bg-[#2E3646] flex justify-center">
+    <div class="py-5 mx-9">
+        <div class="px-3 md:px-5 py-3 md:py-4 bg-[#2E3646] rounded-md flex justify-between items-center">
             <img src="{{ asset('images/Paragraph container.png') }}" class="w-2/3 md:w-[90%]" />
+            <div class="relative">
+                <i class="fa-solid fa-bell text-[#FF7519] text-[24px]"></i>
+                
+                <span class="absolute -top-2 -right-2 bg-black border-2 border-white text-white rounded-full text-xs px-1 py-0.25">5</span>
+            </div>
         </div>
     </div>
 
@@ -9,10 +14,7 @@
         <p class="text-base md:text-lg font-semibold px-9 mb-4 uppercase">Main Menu</p>
 
         @foreach ($menuItems as $menuItem)
-            <div class="text_Style text-lg md:text-xl font-semibold px-9 py-3 md:py-5 flex items-center space-x-4 cursor-pointer"
-                {{-- [ngClass]="{ active: activeIndex === i }"
-            (click)="setActiveIndex(i); item.label === 'Sign Out' ? signOut() : null"
-            [routerLink]="item.label === 'Sign Out' ? null : item.route" --}}>
+            <div class="text_Style text-lg md:text-xl font-semibold px-9 py-3 md:py-5 flex items-center space-x-4 cursor-pointer">
                 <i class="{{ $menuItem['icon'] }}"></i>
                 <a class="no-underline ml-3">{{ $menuItem['label'] }}</a>
             </div>
