8b9965e7c4d6671793bf0b1647773fb1088f1efd
Rana Elgohary <r.elgohary2000@gmail.com>
2024-09-20 11:44:47 +0300

    Data

--- resources/views/pages/teacher/Assignment/details.blade.php
+++ resources/views/pages/teacher/Assignment/details.blade.php
@@ -11,16 +11,26 @@
         'point' => '95',
         'dueDate' => '2024-10-01',
         'topic' => 'Mathematics',
-        'assignTo' => 'Class A',
+        'assignTo' => ['Class A', 'Class B'],
         'title' => 'Homework Assignment',
         'description' => 'Solve the problems in chapter 5.',
-        'uploadedFileName' => 'homework.pdf', // Example of an uploaded file name
+        'uploadedFileName' =>
+        [
+            ['type' => 'photo', 'url' => 'images/Layer 2.png'],
+            ['type' => 'photo', 'url' => 'images/Layer 2.png'],
+            ['type' => 'video', 'url' => '/path/to/video.mp4'],
+            ['type' => 'video', 'url' => '/path/to/video.mp4'],
+            ['type' => 'pdf', 'url' => '/path/to/file.pdf', 'file_name' => 'HannahBusing_Resume.pdf', 'file_space' => '200 KB'],
+            ['type' => 'pdf', 'url' => '/path/to/file.pdf', 'file_name' => 'HannahBusing_Resume.pdf', 'file_space' => '200 KB'],
+            ['type' => 'pdf', 'url' => '/path/to/file.pdf', 'file_name' => 'HannahBusing_Resume.pdf', 'file_space' => '200 KB'],
+            ['type' => 'link', 'url' => 'https://example.com']
+        ],
     ];
 
     $paths = [
       ["name" => "Assignment", "url" => "teacher.Assignment"],
       ["name" => "AssignmentName", "url" => "teacher.assignment.show"],
-    ]; // Example of paths@endphp
+    ];
 
 
 
