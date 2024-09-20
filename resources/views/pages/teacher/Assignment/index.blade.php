8feebf70e64c3b484c6ca0afe51d09159e70b85f
menna osama <menna osama@gmail.com>
2024-09-20 11:02:39 +0300

    teacher route done

--- resources/views/pages/teacher/Assignment/index.blade.php
+++ resources/views/pages/teacher/Assignment/index.blade.php
@@ -31,17 +31,22 @@ $tableData = [
 @section("InsideContent")
 
 
-<div class="p-4">
+<div class="">
 
 
-  <div class="flex justify-between p-3">
+  <div class="flex justify-between">
 
 
     @include('components.path', ['paths' => $paths])
 
-    <button class="w-[99.13px] h-[60.47px] rounded-[11.23px] py-[11.23px] px-[21.06px] bg-[#17253E] text-white border-none">
-        Create
-    </button>
+    <!-- Create Button Wrapped in Link -->
+    <a href="{{ route('teacher.Assignment.create') }}">
+        <button class="w-[99.13px] h-[55.47px] rounded-[11.23px] py-[11.23px] px-[21.06px] bg-[#17253E] text-white border-none mt-4">
+            Create
+        </button>
+        
+    </a>
+
 
 
   </div>
