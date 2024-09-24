118c6eb97cbcda02a87caf5e868974d2fc468a81
= <=>
2024-09-24 16:39:08 +0300

    Theme in student

--- resources/views/pages/student/theme/index.blade.php
+++ resources/views/pages/student/theme/index.blade.php
@@ -15,8 +15,9 @@
     @include('components.sidebar', ['menuItems' => $menuItems])
 @endsection
 
+
 @section('content')
-    <div class="p-5">
+    <div class="p-3">
         <div class="rounded-lg flex items-center justify-between py-3 px-6 bg-[#2E3646]">
             <div class="flex items-center space-x-4">
                 <div>
@@ -49,30 +50,107 @@
         </div>
         @yield('insideContent')
     </div>
-    <div class="p-2 text-[#667085] my-8">
+    <div class="p-3 text-[#667085] my-8">
         <i class="fa-solid fa-house mx-2"></i>
         {{-- @foreach ($paths as $item) --}}
         <span class="mx-2 text-[#D0D5DD]">/</span>
         <a href="#" class="mx-2 cursor-pointer">Theme</a>
         {{-- @endforeach --}}
     </div>
-    <div class="flex flex-wrap">
+    <div class="p-3 flex flex-wrap justify-between">
         @foreach ($materials as $material)
-            <div class="w-full sm:w-1/2 lg:w-1/4 p-2">
-                <div class="h-[350px] bg-white shadow-md border border-slate-200 rounded-md">
-                    <a class="cursor-pointer" href="{{ route('student_units.index', $material->id) }}">
+            <div class="mb-7 w-full md:w-[45%] lg:w-[30%] p-2 bg-white shadow-md rounded-xl min-h-[380px]">
+                <div class="h-full">
+                    <a class="cursor-pointer h-full flex flex-col justify-between" href="{{ route('student_units.index', $material->id) }}">
                         @if ($material->image)
                             <img src="{{ asset('storage/' . $material->image) }}" alt="{{ $material->title }}"
-                                class="object-cover w-full h-[80%] rounded-md">
+                                class="object-cover w-full h-[70%] rounded-xl">
                         @else
                             No Image
                         @endif
-                        <p class="py-5 px-2 text-slate-800 text-2xl font-semibold">
-                            {{ $material->title }}
-                        </p>
+                        <div class="text-slate-800">
+                            <div class="flex justify-between items-center text-2xl">
+                                <p class="font-semibold">{{ $material->title }}</p>
+                                <button class="pt-2" onclick="event.stopPropagation(); event.preventDefault(); openModal('ebook');">
+                                    <i class="fi fi-rr-rectangle-list"></i>
+                                </button>
+                            </div>
+                            <div class="flex justify-between items-center mt-4">
+                                <div>
+                                    <button class="bg-[#17253E] p-2 text-white rounded-md" onclick="event.stopPropagation(); event.preventDefault(); openModal('use');">
+                                        How To Use
+                                    <button>
+                                </div>
+                                <div> 
+                                    <button class="bg-white border border-[#FF7519] p-2 text-black font-semibold rounded-md" onclick="event.stopPropagation(); event.preventDefault(); openModal('learn');">
+                                        Learning Outcomes
+                                    <button>
+                                </div>
+                            </div>
+                        </div>
                     </a>
                 </div>
             </div>
         @endforeach
     </div>
 @endsection
+
+{{-- ------------------------------------------------------------------------------------- --}}
+
+{{-- Ebook Modal --}}
+<div id="ebook" class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center hidden">
+    <div class="bg-white rounded-lg shadow-lg w-1/3">
+        <div class="px-6 py-4 border-b border-gray-200 flex justify-between items-center">
+            <h3 class="text-lg font-semibold text-gray-900">
+                EBook
+            </h3>
+        </div>
+        {{-- Put the EBook Here --}}
+        <div class="px-6 py-4 border-t border-gray-200 flex justify-end">
+            <button onclick="closeModal('ebook')" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mr-2">Close</button>
+        </div>
+    </div>
+</div>
+
+{{-- How To Use Modal --}}
+<div id="use" class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center hidden">
+    <div class="bg-white rounded-lg shadow-lg w-1/3">
+        <div class="px-6 py-4 border-b border-gray-200 flex justify-between items-center">
+            <h3 class="text-lg font-semibold text-gray-900">
+                How To Use
+            </h3>
+        </div>
+        {{-- Put the How To Use Here --}}
+        <div class="px-6 py-4 border-t border-gray-200 flex justify-end">
+            <button onclick="closeModal('use')" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mr-2">Close</button>
+        </div>
+    </div>
+</div>
+
+{{-- Learning Modal --}}
+<div id="learn" class="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center hidden">
+    <div class="bg-white rounded-lg shadow-lg w-1/3">
+        <div class="px-6 py-4 border-b border-gray-200 flex justify-between items-center">
+            <h3 class="text-lg font-semibold text-gray-900">
+                Learning
+            </h3>
+        </div>
+        {{-- Put the Learning Here --}}
+        <div class="px-6 py-4 border-t border-gray-200 flex justify-end">
+            <button onclick="closeModal('learn')" class="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mr-2">Close</button>
+        </div>
+    </div>
+</div>
+
+
+<script>
+
+    function openModal(id) {
+        document.getElementById(id).classList.remove("hidden");
+    }
+
+    function closeModal(id) {
+        document.getElementById(id).classList.add("hidden");
+    }
+
+</script>
