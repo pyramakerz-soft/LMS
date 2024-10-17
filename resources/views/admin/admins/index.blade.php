0005412ce9bcd91b9be793cbc2e9a3b75690d8a8
mennausama <mennausama1682@gmail.com>
2024-10-17 10:29:18 +0300

    image in teacher

--- resources/views/admin/admins/index.blade.php
+++ resources/views/admin/admins/index.blade.php
@@ -17,7 +17,7 @@
                         <div class="alert alert-success">{{ session('success') }}</div>
                     @endif
 
-                    <a href="{{ route('admins.create') }}" class="btn btn-primary mb-3">Add Schools</a>
+                    <a href="{{ route('admins.create') }}" class="btn btn-primary mb-3">Add School</a>
 
                     <div class="table-responsive" style="overflow-x: auto;">
                         <table class="table table-bordered">
