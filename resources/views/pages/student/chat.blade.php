acb18d8fe24c4b855e81ffffe40600ea105c44e5
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2025-01-13 14:23:48 +0200

    edit chat

--- resources/views/pages/student/chat.blade.php
+++ resources/views/pages/student/chat.blade.php
@@ -120,7 +120,7 @@
             const sendButton = chatForm.querySelector('button[type="submit"]');
             sendButton.disabled = true;
 
-            fetch(`/chat/{{ $receiver->id }}/{{ $receiverType }}`, {
+            fetch(`https://pyramakerz-artifacts.com/LMS/lms_pyramakerz/public/chat/{{ $receiver->id }}/{{ $receiverType }}`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -156,7 +156,8 @@
         });
 
         setInterval(function() {
-            fetch(`https://pyramakerz-artifacts.com/LMS/lms_pyramakerz/public/chat/{{ $receiver->id }}/{{ $receiverType }}/messages?last_message_id=${lastMessageId}`)
+            fetch(
+                    `https://pyramakerz-artifacts.com/LMS/lms_pyramakerz/public/chat/{{ $receiver->id }}/{{ $receiverType }}/messages?last_message_id=${lastMessageId}`)
                 .then(response => {
                     if (!response.ok) {
                         throw new Error('Failed to fetch messages');
