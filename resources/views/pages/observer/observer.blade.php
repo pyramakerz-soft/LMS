85968f4c6648fa8ba0c85e0ce67cd95897b438f2
ahmedelragal <ahmed.elragal02@gmail.com>
2025-01-16 11:21:12 +0200

    filter

---
+++ resources/views/pages/observer/observer.blade.php
@@ -0,0 +1,202 @@
+@extends('layouts.app')
+
+@section('title')
+Observer Dashboard
+@endsection
+
+@php
+$menuItems = [
+['label' => 'Observations', 'icon' => 'fi fi-rr-table-rows', 'route' => route('observer.dashboard')],
+['label' => 'Observations Report', 'icon' => 'fi fi-rr-table-rows', 'route' => route('observer.report')],
+];
+@endphp
+
+@section('sidebar')
+@include('components.sidebar', ['menuItems' => $menuItems])
+@endsection
+
+@section('content')
+<div class="p-3 text-[#667085] my-8" style="padding:20px">
+    <div class="flex justify-between items-center mb-6">
+        <h1 class="text-2xl font-bold">Observations</h1>
+        <div class="flex" style="justify-content:center; align-items:center;gap:15px">
+            <!-- Trigger for Filter Modal -->
+            <button id="filter-modal-btn" class="px-4 py-2 text-white rounded-md hover:bg-blue-700" style="background-color:#667085;">
+                Filters
+            </button>
+            <a href="{{ route('observer.observation.create') }}" class="px-4 py-2 text-white rounded-md hover:bg-blue-700" style="background-color:#667085; display:block">
+                Add New Observation
+            </a>
+        </div>
+    </div>
+
+    @if (session('success'))
+    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+        <span class="block sm:inline">{{ session('success') }}</span>
+        <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3 text-green-700" onclick="this.parentElement.remove();">
+            <svg class="fill-current h-6 w-6 text-green-700" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <title>Close</title>
+                <path d="M14.348 5.652a1 1 0 10-1.414-1.414L10 7.586 7.066 4.652a1 1 0 10-1.414 1.414L8.586 10l-2.93 2.934a1 1 0 101.414 1.414L10 12.414l2.934 2.934a1 1 0 101.414-1.414L11.414 10l2.934-2.934z" />
+            </svg>
+        </button>
+    </div>
+    @endif
+    @if (session('error'))
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+        <span class="block sm:inline">{{ session('error') }}</span>
+        <button type="button" class="absolute top-0 bottom-0 right-0 px-4 py-3 text-red-700" onclick="this.parentElement.remove();">
+            <svg class="fill-current h-6 w-6 text-red-700" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <title>Close</title>
+                <path d="M14.348 5.652a1 1 0 10-1.414-1.414L10 7.586 7.066 4.652a1 1 0 10-1.414 1.414L8.586 10l-2.93 2.934a1 1 0 101.414 1.414L10 12.414l2.934 2.934a1 1 0 101.414-1.414L11.414 10l2.934-2.934z" />
+            </svg>
+        </button>
+    </div>
+    @endif
+
+    <div class="overflow-x-auto w-full">
+        <table class="w-full border border-gray-300 text-sm text-left">
+            <thead class="bg-gray-100">
+                <tr>
+                    <th class="border px-4 py-2">Observation Name</th>
+                    <!-- <th class="border px-4 py-2">Observer Name</th> -->
+                    <th class="border px-4 py-2">Teacher Name</th>
+                    <th class="border px-4 py-2">Co-Teacher Name</th>
+                    <th class="border px-4 py-2">School</th>
+                    <th class="border px-4 py-2">City</th>
+                    <th class="border px-4 py-2">Date of Observation</th>
+                    <th class="border px-4 py-2" style="width: 15%;">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($observations as $observation)
+                <tr>
+                    @php
+                    $school = App\Models\School::find($observation->school_id);
+                    @endphp
+                    <td class="border px-4 py-2">{{ $observation->name }}</td>
+                    <!-- <td class="border px-4 py-2">{{ App\Models\Observer::find($observation->observer_id)->name }}</td> -->
+                    <td class="border px-4 py-2">{{ $observation->teacher_name }}</td>
+                    <td class="border px-4 py-2">{{ $observation->coteacher_name }}</td>
+                    <td class="border px-4 py-2">{{ $school->name }}</td>
+                    <td class="border px-4 py-2">{{ $school->city }}</td>
+                    <td class="border px-4 py-2">{{ $observation->activity }}</td>
+                    <td class="border px-4 py-2" style="text-align:center; ">
+                        <a href="{{ route('observation.view', $observation->id) }}"
+                            class="text-white font-medium py-2 px-4 rounded shadow focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-50" style="background-color:#667085; margin-right:5px;">
+                            <i class="fa-solid fa-eye"></i>
+                        </a>
+                        <form action="{{ route('observation.destroy', $observation->id) }}" method="POST" style="display:inline-block;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                class="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded shadow focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-50"
+                                onclick="return confirm('Are you sure you want to delete this Observation?');">
+                                <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="6" class="border px-4 py-2 text-center">No observations found.</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+<!-- Filter Modal -->
+<div id="filter-modal" class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center hidden">
+    <div
+        class="bg-white rounded-lg shadow-lg p-6 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 max-h-[90vh] overflow-y-auto">
+        <h2 class="text-xl font-bold mb-4">Filters</h2>
+        <form id="filter-form-modal" action="{{ route('observer.dashboard') }}" method="GET">
+            <div class="mb-4">
+                <label for="teacher_id_modal" class="block text-sm font-medium text-gray-700">Teacher</label>
+                <select name="teacher_id" id="teacher_id_modal" class="w-full p-2 border border-gray-300 rounded">
+                    <option value="">All Teachers</option>
+                    @foreach ($teachers as $teacher)
+                    <option value="{{ $teacher->id }}" {{ request('teacher_id') == $teacher->id ? 'selected' : '' }}>
+                        {{ $teacher->name }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-4">
+                <label for="school_id" class="block text-sm font-medium text-gray-700">School</label>
+                <select name="school_id[]" id="school_id" class="w-full p-2 border border-gray-300 rounded" multiple>
+                    <!-- <option value="" disabled selected>Select Schools</option> -->
+                    @foreach ($schools as $school)
+                    <option value="{{ $school->id }}"
+                        {{ is_array(request('school_id')) && in_array($school->id, request('school_id')) ? 'selected' : '' }}>
+                        {{ $school->name }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-4">
+                <label for="city" class="block text-sm font-medium text-gray-700">City</label>
+                <select name="city[]" id="city" class="w-full p-2 border border-gray-300 rounded" multiple>
+                    <!-- <option value="" disabled selected>Select Schools</option> -->
+                    @foreach ($cities as $city)
+                    <option value="{{ $city }}"
+                        {{ is_array(request('city')) && in_array($city, request('city')) ? 'selected' : '' }}>
+                        {{ $city }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="mb-4">
+                <label for="school_id" class="block text-sm font-medium text-gray-700">Stage</label>
+                <select name="stage_id" id="stage_id" class="w-full p-2 border border-gray-300 rounded">
+                    <option value="">All Stages</option>
+                    @foreach ($stages as $stage)
+                    <option value="{{ $stage->id }}" {{ request('stage_id') == $stage->id ? 'selected' : '' }}>
+                        {{ $stage->name }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-4">
+                <label for="lesson_segment_filter" class="block text-sm font-medium text-gray-700">Lesson Segment</label>
+                <select class="w-full p-2 border border-gray-300 rounded" name="lesson_segment_filter" id="lesson_segment_filter">
+                    <option value="">All</option>
+                    <option value="Beginning" {{ request('lesson_segment_filter') == 'Beginning' ? 'selected' : '' }}>Beginning</option>
+                    <option value="Middle" {{ request('lesson_segment_filter') == 'Middle' ? 'selected' : '' }}>Middle</option>
+                    <option value="End" {{ request('lesson_segment_filter') == 'End' ? 'selected' : '' }}>End</option>
+                </select>
+            </div>
+            <div class="mb-4">
+                <label for="from_date" class="block text-sm font-medium text-gray-700">Date From</label>
+                <input type="date" name="from_date" id="from_date" class="w-full p-2 border border-gray-300 rounded" value="{{ request('from_date') }}">
+            </div>
+            <div class="mb-4">
+                <label for="to_date" class="block text-sm font-medium text-gray-700">Date To</label>
+                <input type="date" name="to_date" id="to_date" class="w-full p-2 border border-gray-300 rounded" value="{{ request('to_date') }}">
+            </div>
+            <input type="checkbox" name="include_comments" value="1" {{ request('include_comments') ? 'checked' : '' }}> Includes Comments
+            <div class="flex justify-end">
+                <button type="button" id="close-modal-btn" class="px-4 py-2 bg-gray-500 text-white rounded mr-2">Close</button>
+                <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Apply Filters</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
+
+@section('page_js')
+<script>
+    // Modal toggle
+    document.getElementById('filter-modal-btn').addEventListener('click', function() {
+        document.getElementById('filter-modal').classList.remove('hidden');
+    });
+
+    document.getElementById('close-modal-btn').addEventListener('click', function() {
+        document.getElementById('filter-modal').classList.add('hidden');
+    });
+</script>
+@endsection
