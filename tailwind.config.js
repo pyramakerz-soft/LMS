27dad5fdea17725337378a489267721af10169e5
pyramakerzgit@gmail.com <pyramakerzgit@gmail.com>
2025-01-19 08:46:07 +0000

    Fix:edit chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 export default {
     content: [
         "./resources/**/*.blade.php",
+<<<<<<< HEAD
         "./resources/**/*.js",
         "./resources/**/*.vue",
         // "./src/**/*.{html , js, jsx, ts , tsx}",
+=======
+        "./resources/**/*.{html , js , jsx , tsx, ts}",
+        "./resources/**/*.vue",
+>>>>>>> c5b5c4e (edit chat)
     ],
     theme: {
         extend: {
