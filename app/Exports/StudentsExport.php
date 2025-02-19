26b89f6d827de087eafaca609821b35e0024c36a
unknown <ahmed.elragal02@gmail.com>
2025-02-19 14:29:24 +0200

    student class export and add ebook

--- app/Exports/StudentsExport.php
+++ app/Exports/StudentsExport.php
@@ -5,6 +5,7 @@ namespace App\Exports;
 use App\Models\Student;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+
 class StudentsExport implements FromCollection, WithHeadings
 {
     protected $classId;
@@ -26,16 +27,22 @@ class StudentsExport implements FromCollection, WithHeadings
             ->with(['school', 'stage'])
             ->get()
             ->map(function ($student) {
+                if ($student->gender == 'Boy' || $student->gender == 'boy') {
+                    $gender = 'ذكر';
+                } else {
+                    $gender = 'انثى';
+                }
                 return [
-                    'username' => $student->username,
-                    'password' => $student->plain_password,
-                    'school_name' => $student->school->name,
-                    'grade' => $student->stage->name,
+                    'username' => str_replace('_', ' ', $student->username) ?? '',
+                    'arabic_name' => $student->arabic_name ?? '',
+                    'gender' => $gender ?? '',
+                    'phone' => (string)$student->phone ?? '',
+                    'birth_date' => $student->birth_date ?? '',
                 ];
             });
     }
     public function headings(): array
     {
-        return ['Username', 'Password', 'School Name', 'Grade'];
+        return ['اسم الطالب باللغة الانجليزية', 'اسم الطالب باللغة العربية', 'الجنس', 'رقم الموبايل', 'تاريخ الميلاد'];
     }
 }
