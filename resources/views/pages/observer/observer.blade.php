de6fa6e19ec06ccd1dd32e2a89d35bfe2cc2823e
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2025-03-11 11:26:29 +0200

    edit btn delete

--- resources/views/pages/observer/observer.blade.php
+++ resources/views/pages/observer/observer.blade.php
@@ -105,6 +105,7 @@
                                     @method('DELETE')
                                     <button type="submit"
                                         class="btn btn-danger bg-danger bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded shadow focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-50"
+                                        style="background-color: #f00"
                                         onclick="return confirm('Are you sure you want to delete this Observation?');">
                                         <i class="fa-solid fa-trash"></i>
                                     </button>
