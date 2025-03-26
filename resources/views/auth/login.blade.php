16a49c14303a5303ae5761ac368d00e230f60084
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2025-03-26 13:23:43 +0200

    add login as in login page and fix ui in observer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,13 +24,14 @@
 <body>
 
     <div class="lg:h-[100vh] flex flex-col-reverse lg:flex-row">
-        <form action="{{route('login.post')}}" method="POST" class="w-full lg:w-1/3 bg-white flex flex-col justify-center items-center p-12 md:p-20">
+        <form action="{{ route('login.post') }}" method="POST"
+            class="w-full lg:w-1/3 bg-white flex flex-col justify-center items-center p-12 md:p-20">
             @csrf
             <img src="{{ asset('images/Paragraphcontainer.png') }}" alt="Logo" class="w-[63%]">
             <p class="h5 text-[#111111] py-3">Hello, please sign into your account</p>
 
             @if ($errors->has('username') || $errors->has('password'))
-            <span style="color: red" class="text-red-500 text-xs">Invalid Username Or Password</span>
+                <span style="color: red" class="text-red-500 text-xs">Invalid Username Or Password</span>
             @endif
 
             <div class="w-full text-start">
@@ -38,29 +39,40 @@
                     Username
                 </label>
                 <input id="email" name="username" type="text" placeholder="Enter your username"
-                    class="h5 w-full bg-white border rounded border-[#D0D5DD] shadow-sm shadow-[#1018280D] h-[22%]  py-[7.5px] px-[10.5px]" [(ngModel)]="email">
+                    class="h5 w-full bg-white border rounded border-[#D0D5DD] shadow-sm shadow-[#1018280D] h-[22%]  py-[7.5px] px-[10.5px]"
+                    [(ngModel)]="email">
 
                 <label htmlFor="pass" class="h-4 font-medium text-[#344054] block mb-1 mt-4">
                     Password
                 </label>
                 <input id="pass" type="password" name="password"
-                    class="h5 w-full bg-white border rounded border-[#D0D5DD] shadow-sm shadow-[#1018280D] h-[22%] py-[7.5px] px-[10.5px]" [(ngModel)]="password">
+                    class="h5 w-full bg-white border rounded border-[#D0D5DD] shadow-sm shadow-[#1018280D] h-[22%] py-[7.5px] px-[10.5px]"
+                    [(ngModel)]="password">
             </div>
-
-            <button type="submit" class="mt-4 w-full text-white font-semibold text-xs shadow-sm bg-[#17253E] border border-[#17253E] py-2 rounded-md">Sign in</button>
+            <label class="text-left w-full font-medium">Login As</label>
+            <div class="w-full mb-4 ">
+                <label><input type="radio" name="login_as" value="student" required> Student</label><br>
+                <label><input type="radio" name="login_as" value="teacher"> Teacher</label><br>
+                <label><input type="radio" name="login_as" value="observer"> Observer</label>
+            </div>
+            <button type="submit"
+                class="mt-4 w-full text-white font-semibold text-xs shadow-sm bg-[#17253E] border border-[#17253E] py-2 rounded-md">Sign
+                in</button>
 
             @if ($errors->has('credentials'))
-            <span class="text-red-500 text-xs mt-2">{{ $errors->first('credentials') }}</span>
+                <span class="text-red-500 text-xs mt-2">{{ $errors->first('credentials') }}</span>
             @endif
         </form>
 
         <div class="w-full lg:w-2/3 bgLinearGradient text-white flex flex-col justify-center ">
             <div class="mx-9 p-3 lg:p-0 text-center">
-                <p class="text-xl md:text-4xl font-bold mb-8">Do more <span class="font-normal">with Pyramakerz</span></p>
+                <p class="text-xl md:text-4xl font-bold mb-8">Do more <span class="font-normal">with Pyramakerz</span>
+                </p>
 
                 <div class="flex flex-col items-center justify-center">
                     <img src="{{ asset('images/login.png') }}" alt="calender" class="w-[83%] mb-6">
-                    <p>Welcome to the Learning Management System (LMS) login screen! Here, you can access a world of educational resources designed to enhance your learning experience. </p>
+                    <p>Welcome to the Learning Management System (LMS) login screen! Here, you can access a world of
+                        educational resources designed to enhance your learning experience. </p>
                 </div>
             </div>
         </div>
