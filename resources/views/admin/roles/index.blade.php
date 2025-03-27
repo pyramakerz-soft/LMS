4e968d925e8591dd3589604b63077f8b12558972
muhammedkhaled01 <mohamedkhaled667.mk@gmail.com>
2025-03-27 12:48:27 +0200

    edit role

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -15,7 +15,9 @@
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-2">
                         <div class="flex justify-end p-2">
-                            <a href="{{ route('admin.roles.create') }}" class="btn btn-success">Create Role</a>
+                            @can('create role')
+                                <a href="{{ route('admin.roles.create') }}" class="btn btn-success">Create Role</a>
+                            @endcan
                         </div>
                         <div class="flex flex-col">
                             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -43,18 +45,22 @@
                                                         <td>
                                                             <div class="d-flex justify-content-end">
                                                                 <div class="d-flex align-items-center">
-                                                                    <a href="{{ route('admin.roles.edit', $role->id) }}"
-                                                                        class="btn btn-primary">Edit</a>
-                                                                    <form
-                                                                        class="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-md"
-                                                                        method="POST"
-                                                                        action="{{ route('admin.roles.destroy', $role->id) }}"
-                                                                        onsubmit="return confirm('Are you sure?');">
-                                                                        @csrf
-                                                                        @method('DELETE')
-                                                                        <button type="submit"
-                                                                            class="btn btn-danger">Delete</button>
-                                                                    </form>
+                                                                    @can('update role')
+                                                                        <a href="{{ route('admin.roles.edit', $role->id) }}"
+                                                                            class="btn btn-primary">Edit</a>
+                                                                    @endcan
+                                                                    @can('delete role')
+                                                                        <form
+                                                                            class="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-md"
+                                                                            method="POST"
+                                                                            action="{{ route('admin.roles.destroy', $role->id) }}"
+                                                                            onsubmit="return confirm('Are you sure?');">
+                                                                            @csrf
+                                                                            @method('DELETE')
+                                                                            <button type="submit"
+                                                                                class="btn btn-danger">Delete</button>
+                                                                        </form>
+                                                                    @endcan
                                                                 </div>
                                                             </div>
                                                         </td>
