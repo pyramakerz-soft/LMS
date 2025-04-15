7f6a32c8efb81d22f4f9857c007e053dc2b2c8a2
pyramakerz-soft <pyramakerzgit@gmail.com>
2025-04-15 12:39:21 +0200

    Update 01_fix_permissions.sh

--- .platform/hooks/postdeploy/01_fix_permissions.sh
+++ .platform/hooks/postdeploy/01_fix_permissions.sh
@@ -7,4 +7,4 @@ chmod -R 755 /var/app/current/public
 chmod -R 775 /var/app/current/storage
 chmod -R 775 /var/app/current/bootstrap/cache
 
-echo "[POSTDEPLOY] Permissions fixed."
+echo "[POSTDEPLOY] Permissions fixed"
